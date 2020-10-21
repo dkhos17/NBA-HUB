cb82b048b897be28fbd1c5603a610120a585bade
dkhos17 <dkhos17@freeuni.edu.ge>
2020-10-21 22:56:20 +0400

    add slideshow

---
+++ back-js/slideshow.js
@@ -0,0 +1,32 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var slides = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("dots");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (var i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    
+    dots[slideIndex - 1].className += " active";
+    slides[slideIndex - 1].style.display = "block";
+}
